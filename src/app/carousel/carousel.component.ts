1d3cb5db08618dc9aadfa10c3e81d946722d1f8a
Balázs Bencs <balazs.bencs@ibm.com>
2018-06-22 16:45:07 +0200

    Add typings

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 
+import * as data from '../../assets/i18n/en.json';
+
 @Component({
   selector: 'app-carousel',
   template: `
@@ -18,15 +20,20 @@ import { map } from 'rxjs/operators';
 })
 export class CarouselComponent implements OnInit {
   images: Array<string>;
-  slides;
+  slides = (<any>data).header.carousel.length;
+  map: Array<number>;
   constructor(private _http: HttpClient) { }
   ngOnInit() {
+    for (var i=1;i<=this.slides;i++){
+      map[i] = i;
+      console.log(i);
+    }
     this._http.get('https://picsum.photos/list')
       .pipe(map((images: Array<{ id: number }>) => this._randomImageUrls(images)))
       .subscribe(images => this.images = images);
   }
   private _randomImageUrls(images: Array<{ id: number }>): Array<string> {
-    return [1, 2, 3].map(() => {
+    return [map].map(() => {
       const randomId = images[Math.floor(Math.random() * images.length)].id;
       return `https://picsum.photos/900/500?image=${randomId}`;
     });
